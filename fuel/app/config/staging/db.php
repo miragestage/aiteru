344d0741f24a37705d27798f29c15ccd03e063ac
maruei <ag-mail@nirai.ne.jp>
2013-11-09 14:38:11 +0900

    staging db

--- fuel/app/config/staging/db.php
+++ fuel/app/config/staging/db.php
@@ -6,9 +6,9 @@
 return array(
 	'default' => array(
 		'connection'  => array(
-			'dsn'        => 'mysql:host=localhost;dbname=fuel_staging',
-			'username'   => 'fuel_app',
-			'password'   => 'super_secret_password',
+			'dsn'        => 'mysql:host=10.0.61.235;dbname=aiteru',
+			'username'   => 'piwFC79droAXy',
+			'password'   => 'uDvbehFwsbtKw',
 		),
 	),
 );
