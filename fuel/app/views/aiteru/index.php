b2b2b7b565ff4033a78ec039ae62b4d58f1d3def
maruei <ag-mail@nirai.ne.jp>
2013-11-09 14:07:31 +0900

    syusei

--- fuel/app/views/aiteru/index.php
+++ fuel/app/views/aiteru/index.php
@@ -1,1 +1,19 @@
-<?php echo "aaaaaaこんにちわ"; ?>
+<?php
+ 
+$services_json = getenv('VCAP_SERVICES');
+$services = json_decode($services_json, true);
+$config = null;
+foreach ($services as $name => $service) {
+    if (0 === stripos($name, 'mysql')) {
+        $config = $service[0]['credentials'];
+        break;
+    }
+}
+is_null($config) && die('MySQL service information not found.');
+ 
+$db_hostname = $config["hostname"];
+$db_hostport = $config["port"];
+$db_username = $config["user"];
+$db_password = $config["password"];
+
+echo $db_hostname;
